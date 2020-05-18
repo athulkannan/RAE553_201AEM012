edc87f8a6ce3cf16e5412cd6bf8c14c7f7d02d0f
athulkannan <athul.skannan@gmail.com>
2020-05-18 16:09:39 +0300

    These are the final versions of the following project files:
    app.py, create_table.py, item.py, security.py and user.py

--- app.py
+++ app.py
@@ -1,50 +1,21 @@
-from flask import Flask, request
-from flask_restful import Resource, Api, reqparse
-from flask_jwt import JWT, jwt_required
+from flask import Flask
+from flask_restful import Api
+from flask_jwt import JWT
 
 from security import authenticate, identity
+from user import UserRegister
+from item import Item, ItemList
 
 app = Flask(__name__)
 app.secret_key = 'yalayolo'
 api = Api(app)
 
-items = []
-
-class Item(Resource):
-    def get(self, name):
-        for item in items:
-            if item['name'] == name:
-                return item
-        return {'item' : None}, 404
-
-    def post(self, name):
-        data = request.get_json()
-        item = {'name' : name, 'price' : data['price']}
-        items.append(item)
-        return item, 201
-    def put(self, name):
-        parser =  reqparse.RequestParser()
-        parser.add_argument('price',
-            type = float,
-            required = True,
-            help = 'This field cannot be left blank!'
-        )
-
-class Itemlist(Resource):
-    def get(self):
-        return {'items' : items}
+jwt = JWT(app, authenticate, identity)
 
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(Itemlist, '/items')
+api.add_resource(UserRegister, '/register')
 
-jwt = JWT(app, authenticate, identity)
-
-@app.route('/auth')
-@jwt_required()
-def auth():
-    return '%s' % current_identity
-
-if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+app.run(port=5000, debug=True)
 
 #RAE553_201AEM012
